9ebb12e00f616a4f603d7e45ff75f79a64adbe47
SWAMESHA <swamesha444@gmail.com>
2024-09-12 17:15:47 +0530

    swamy push

---
+++ src/main/java/com/example/codingShuttle/SecurityApp/Security_Application/swamy/Swamy.java
@@ -0,0 +1,7 @@
+package com.example.codingShuttle.SecurityApp.Security_Application.swamy;
+
+public class Swamy {
+    public static void main(String[] args) {
+        System.out.println("Swamy");
+    }
+}
